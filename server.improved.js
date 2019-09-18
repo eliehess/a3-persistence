67244987ef4876af2a5da6b73229e6201b16d108
Elie <eliemhess@gmail.com>
2019-09-18 14:39:39 -0400

    Delete unnecessary data

--- server.improved.js
+++ server.improved.js
@@ -18,15 +18,8 @@ const low = require('lowdb'),
   dir = "public/",
   port = 3000;
 
-
-/*app.get("/", (request, response) => {
-  //response.send("Hello, World!")
-  response.sendFile(__dirname + "/public/index.html");
-});*/
-
 app.use(express.static('public'));
 app.use(compression());
-//app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cookieParser());
 app.use(function(err, req, res, next) {
@@ -138,8 +131,7 @@ app.post('/login', passport.authenticate('local'), function(req, res) {
 })*/
 
 app.get('/', function(req, res) {
-  //console.log(req.user)
-  res.sendFile(__dirname + '/public/login.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/getdata', function(req, res) {
@@ -171,7 +163,7 @@ app.post('/update', function(req, res) {
       "password": updatedData.password
     };
     db.get('members').remove({
-      uuid: updatedData.uuid
+      username: updatedData.username
     }).write()
 
     db.get('members').push(updatedEntry).write();
@@ -228,140 +220,6 @@ app.post('/delete', function(req, res) {
     res.end();
   })
 });
-
-/*db.defaults({
-  members: [{
-      "firstname": "Luke",
-      "lastname": "Skywalker",
-      "major": "Lightsaber Construction",
-      "uuid": "f5701299-05f8-4ca0-a904-fa6fdfd3718c",
-      "username": "Luke",
-      "password": "Skywalker"
-    },
-    {
-      "firstname": "Obi-Wan",
-      "lastname": "Kenobi",
-      "major": "Taking the High Ground",
-      "uuid": "524dee4e-b72e-49e2-adf2-e4907db45fda",
-      "username": "Obi-Wan",
-      "password": "Kenobi"
-    }
-  ]
-}).write();
-
-const server = http.createServer(function(request, response) {
-  if (request.method === "GET") {
-    handleGet(request, response);
-  } else if (request.method === "POST") {
-    handlePost(request, response);
-  }
-});
-
-const handleGet = function(request, response) {
-  const filename = dir + request.url.slice(1);
-  if (request.url === "/") {
-    //sendFile(response, "public/index.html")
-    sendFile(response, "public/login.html")
-  } else if (request.url === "/getdata") {
-    sendData(response, db.get('members').value() );
-  } else {
-    sendFile(response, filename);
-  }
-};
-
-const handlePost = function(request, response) {
-  let dataString = "";
-
-  request.on("data", function(data) {
-    dataString += data
-  });
-
-  request.on("end", function() {
-    switch (request.url) {
-      case "/submit":
-        const data = JSON.parse(dataString);
-        const newUUID = uuidv4();
-
-        const newMember = {
-          "firstname": data.firstname,
-          "lastname": data.lastname,
-          "major": data.major,
-          "uuid": newUUID
-        }
-
-        db.get('members').push(newMember).write()
-
-        response.writeHead(200, "OK", {
-          "Content-Type": "text/plain"
-        });
-        response.end();
-
-        break;
-      case "/update":
-        const updatedData = JSON.parse(dataString);
-
-        const updatedEntry = {
-          "firstname": updatedData.firstname,
-          "lastname": updatedData.lastname,
-          "major": updatedData.major,
-          "uuid": updatedData.uuid
-        };
-        db.get('members').remove({
-          uuid: updatedData.uuid
-        }).write()
-
-        db.get('members').push(updatedEntry).write();
-
-        response.writeHead(200, "OK", {
-          "Content-Type": "text/plain"
-        });
-        response.end();
-        break;
-
-      case "/delete":
-        const entryToDelete = JSON.parse(dataString);
-        db.get('members').remove({
-          uuid: entryToDelete.uuid
-        }).write()
-        response.writeHead(200, "OK", {
-          "Content-Type": "text/plain"
-        });
-        response.end();
-        break;
-
-      default:
-        response.end("404 Error: File Not Found");
-        break;
-    }
-  })
-};
-
-const sendData = function(response, request) {
-  const type = mime.getType(request);
-  response.writeHeader(200, {
-    "Content-Type": type
-  });
-  response.write(JSON.stringify({
-    data: request
-  }));
-  response.end();
-};
-
-const sendFile = function(response, filename) {
-  const type = mime.getType(filename);
-
-  fs.readFile(filename, function(err, content) {
-    if (err === null) {
-      response.writeHeader(200, {
-        "Content-Type": type
-      });
-      response.end(content)
-    } else {
-      response.writeHeader(404);
-      response.end("404 Error: File Not Found");
-    }
-  })
-};*/
 
 app.listen(process.env.PORT || port, function() {
   console.log("Server running on port " + port);
