856cc052a3ffbbdb500177357367fa24f9aff8b5
Elie <eliemhess@gmail.com>
2019-09-18 21:06:39 -0400

    Delete unnecessary comments

--- server.improved.js
+++ server.improved.js
@@ -59,7 +59,6 @@ passport.serializeUser((user, done) => done(null, user.username))
 
 passport.deserializeUser((username, done) => {
   const user = db.get("members").find(u => u.username === username)
-  //console.log('deserializing:', user)
 
   if (user !== undefined) {
     done(null, user)
@@ -88,11 +87,7 @@ app.post('/login', passport.authenticate('local', {
   res.json({
     status: true
   })
-})
-
-/*app.post('/login', function(req, res) {
-  console.log("hi there");
-})*/
+});
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -111,22 +106,12 @@ app.get('/getdata', function(req, res) {
 });
 
 app.post('/update', function(req, res) {
-  /*let dataString = "";
-  console.log("update")
-  req.on("data", function(data) {
-    dataString += data
-    console.log(data)
-  });
-  console.log("body: " + req.body)
-  console.log("username: " + req.body.username)*/
-  //const updatedData = req.body;
   const updatedEntry = {
     "firstname": req.body.firstname,
     "lastname": req.body.lastname,
     "username": req.body.username,
     "password": req.body.password
   };
-  console.log(updatedEntry)
 
   db.get('members').remove({
     username: updatedEntry.username
@@ -138,45 +123,9 @@ app.post('/update', function(req, res) {
     "Content-Type": "text/plain"
   });
   res.end();
-
-  /*req.on("end", function() {
-    //const updatedData = JSON.parse(dataString);
-    const updatedEntry = {
-      "firstname": updatedData.firstname,
-      "lastname": updatedData.lastname,
-      "username": updatedData.username,
-      "password": updatedData.password
-    };
-    db.get('members').remove({
-      username: updatedData.username
-    }).write();
-
-    db.get('members').push(updatedEntry).write();
-
-    res.writeHead(200, "OK", {
-      "Content-Type": "text/plain"
-    });
-    res.end();
-  })*/
 });
 
 app.post('/submit', function(req, res) {
-  /*let dataString = "";
-
-  req.on("data", function(data) {
-    dataString += data
-  });
-
-  req.on("end", function() {
-    const data = JSON.parse(dataString);
-
-    const newMember = {
-      "firstname": data.firstname,
-      "lastname": data.lastname,
-      "username": data.username,
-      "password": data.password
-    }*/
-
   const newMember = {
     "firstname": req.body.firstname,
     "lastname": req.body.lastname,
@@ -184,37 +133,18 @@ app.post('/submit', function(req, res) {
     "password": req.body.password
   };
 
-  db.get('members').push(newMember).write()
+  db.get('members').push(newMember).write();
 
   res.writeHead(200, "OK", {
     "Content-Type": "text/plain"
   });
   res.end();
 })
-//});
 
 app.post('/delete', function(req, res) {
-  /*let dataString = "";
-
-  req.on("data", function(data) {
-    dataString += data
-  });
-
-  req.on("end", function() {
-    const entryToDelete = JSON.parse(dataString);
-    db.get('members').remove({
-      //uuid: entryToDelete.uuid
-      username: entryToDelete.username
-    }).write()
-    res.writeHead(200, "OK", {
-      "Content-Type": "text/plain"
-    });
-    res.end();
-  })*/
   db.get('members').remove({
-    //uuid: entryToDelete.uuid
     username: req.body.username
-  }).write()
+  }).write();
   res.writeHead(200, "OK", {
     "Content-Type": "text/plain"
   });
